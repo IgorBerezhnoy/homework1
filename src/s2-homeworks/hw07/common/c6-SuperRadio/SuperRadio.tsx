51749c906915ab825c4b187de0efb28bcef449d0
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-08-07 16:09:32 +0800

    task 7 correcting warning 2

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -51,8 +51,8 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
-                      value={o.value}
-                      name={o.value}
+                      value={value}
+                      name={value?value:o.value}
                       checked={value==o.id}
                       // name, checked, value делают студенты
 
